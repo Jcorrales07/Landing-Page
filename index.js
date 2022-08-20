7a8aa8a64cf3704ee2b74cefe16f2b2e6bf42471
Jcorrales07 <corralesjoe07@gmail.com>
2022-08-19 18:47:00 -0600

    .

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const mobileMenu = document.querySelector(".mobile-btn");
+const closeMobileMenu = document.querySelector(".mobile-btn .close-btn")
+
 
 mobileMenu.addEventListener('touchstart', () => {
-
+    
 })
 console.log(mobileMenu)
