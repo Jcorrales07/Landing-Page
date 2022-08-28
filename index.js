8454cb261f4208f22aee5c831f9d535e71ecf25f
Jcorrales07 <corralesjoe07@gmail.com>
2022-08-28 13:44:55 -0600

    interactividad con el menu

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const menuBtn = document.querySelector('.menu-btn')
+const menuBtn = document.querySelector('.menu-btn');
+const closeMenuBtn = document.querySelector('.close-menu-btn');
+const nav = document.querySelector('.nav');
 
+console.log(nav);
 
 menuBtn.addEventListener('touchstart', () => {
-    alert('me tocaste')
-})
+    // nav.classList.remove('mobile-device');
+    // nav.classList.add('desktop-device');
+    menuBtn.style.display = 'none'
+    nav.style.display = 'flex';
+});
+
+closeMenuBtn.addEventListener('touchstart', () => {
+    menuBtn.style.display = 'block'
+    nav.style.display = 'none';
+});
