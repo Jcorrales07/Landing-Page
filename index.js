997739ea1bd1de55363a4359fe30b99f15df6f0e
Jcorrales07 <corralesjoe07@gmail.com>
2022-09-11 17:52:04 -0600

    termiado

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const menuBtn = document.querySelector('.menu-btn');
 const closeMenuBtn = document.querySelector('.close-menu-btn');
 const nav = document.querySelector('.nav');
-
 const liListShow = document.querySelectorAll('.list__item');
 
-const menuExtension = (event) => {
-    liListShow.forEach((li) => {
+const menuExtension = (event = "click") => {
+    liListShow.forEach((li, i) => {
+        const listShow = li.querySelector('.list__show')
+        const arrow = li.querySelector('.list__arrow')
+        console.log(arrow)
 
-        if (li.querySelector('.list__show')) {
-        
+        if (listShow) {
             li.addEventListener(event, () => {
-                const lista = li.querySelector('.list__show');
-                let listaHeight = lista.clientHeight;
-    
-                lista.style.height =
-                    listaHeight == 0 ? 
-                    `${lista.scrollHeight}px` : `0px`;
+                const listaHeight = listShow.clientHeight;
+                const listScrollHeight = listShow.scrollHeight;
+                arrow.classList.toggle('list__arrow--active');
+
+                listShow.style.height = (listaHeight === 0) ? `${listScrollHeight}px` : `0px`;
+                listShow.style.padding = (listaHeight === 0) ? `0.5rem 1.5rem 1.5rem 1.5rem` : `0px`;
             });
         }
     });
@@ -31,10 +32,8 @@ const closeMenu = () => {
     nav.style.display = 'none';
 };
 
-if(document.body.clientWidth < 1024) {
-    menuExtension('touchstart')
-} else menuExtension('click')
-
+if(document.body.clientWidth < 1024) menuExtension('touchstart');
+else menuExtension();
 
 menuBtn.addEventListener('touchstart', openMenu);
 closeMenuBtn.addEventListener('touchstart', closeMenu);
